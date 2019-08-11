4d87aaef7993fb37b93983d5faeceecb7e10a225
pluginvideoDark27 <48326077+pluginvideoDark27@users.noreply.github.com>
2019-08-11 13:55:33 +0200

    Create httptools.py

---
+++ httptools.py
@@ -0,0 +1,10 @@
+<channels>
+ 
+<channel>
+<name>[COLOR green]httptools.py[/COLOR]</name>
+<description>En este espacio podras disfrutar de Los Mejores Videoclip.</description>
+<thumbnail>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</thumbnail>
+<fanart>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</fanart>
+<externallink></externallink>
+</channel>
+</channels>

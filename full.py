3e2bb45411713a99600f83aba817249dac24d162
pluginvideoDark27 <48326077+pluginvideoDark27@users.noreply.github.com>
2019-08-11 14:06:50 +0200

    Update and rename full.xml to full.py

--- full.py
+++ full.py
@@ -1,7 +1,7 @@
 <channels>
  
 <channel>
-<name>[COLOR green]Videos De Musica [/COLOR]</name>
+<name>[COLOR green]hdfull1.py[/COLOR]</name>
 <description>En este espacio podras disfrutar de Peliculas Y Series</description>
 <thumbnail>https://hdfull.me/templates/hdfull/images/logo.png</thumbnail>
 <fanart>https://www.tuexpertomovil.com/wp-content/uploads/2013/06/moviles-con-pantalla-fullhd-1280x720.jpg</fanart>

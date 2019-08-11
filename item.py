b44a022f4fe3d6e5c002f0d3ffd836d68b700a42
pluginvideoDark27 <48326077+pluginvideoDark27@users.noreply.github.com>
2019-08-11 14:00:05 +0200

    Update item.py

--- item.py
+++ item.py
@@ -3,8 +3,8 @@
 <channel>
 <name>[COLOR green]item.py [/COLOR]</name>
 <description>En este espacio podras disfrutar de Los Mejores Videoclip.</description>
-<thumbnail>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</thumbnail>
+<thumbnail></thumbnail>
 <fanart>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</fanart>
-<externallink></externallink>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/item1.py</externallink>
 </channel>
 </channels>

d2465bf3671dbcb1a51d6df159698d58c93abc88
pluginvideoDark27 <48326077+pluginvideoDark27@users.noreply.github.com>
2019-08-11 14:07:10 +0200

    Create todos.py

---
+++ todos.py
@@ -0,0 +1,58 @@
+<channels>
+ 
+<channel>
+<name>[COLOR green]hdfull1.py[/COLOR]</name>
+<description>En este espacio podras disfrutar de Peliculas Y Series</description>
+<thumbnail>https://hdfull.me/templates/hdfull/images/logo.png</thumbnail>
+<fanart>https://www.tuexpertomovil.com/wp-content/uploads/2013/06/moviles-con-pantalla-fullhd-1280x720.jpg</fanart>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/hdfull.py</externallink>
+</channel>
+</channels>
+
+
+<channels>
+ 
+<channel>
+<name>[COLOR green]item.py [/COLOR]</name>
+<description>En este espacio podras disfrutar de Los Mejores Videoclip.</description>
+<thumbnail></thumbnail>
+<fanart>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</fanart>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/item1.py</externallink>
+</channel>
+</channels>
+
+
+<channels>
+ 
+<channel>
+<name>[COLOR green]httptools.py[/COLOR]</name>
+<description>En este espacio podras disfrutar de Los Mejores Videoclip.</description>
+<thumbnail>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</thumbnail>
+<fanart>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</fanart>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/httptools1.py</externallink>
+</channel>
+</channels>
+
+
+<channels>
+ 
+<channel>
+<name>[COLOR green]scrapertools.py[/COLOR]</name>
+<description>En este espacio podras disfrutar de Los Mejores Videoclip.</description>
+<thumbnail>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</thumbnail>
+<fanart>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</fanart>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/scrapertools1.py</externallink>
+</channel>
+</channels>
+
+
+<channels>
+ 
+<channel>
+<name>[COLOR green]servertools.py[/COLOR]</name>
+<description>En este espacio podras disfrutar de Los Mejores Videoclip.</description>
+<thumbnail>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</thumbnail>
+<fanart>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</fanart>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/servertools1.py</externallink>
+</channel>
+</channels>

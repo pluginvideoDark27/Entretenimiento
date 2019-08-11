126de6a5b40466428eddaebf66b659b5b2403386
pluginvideoDark27 <48326077+pluginvideoDark27@users.noreply.github.com>
2019-08-11 14:01:05 +0200

    Update scrapertools.py

--- scrapertools.py
+++ scrapertools.py
@@ -5,6 +5,6 @@
 <description>En este espacio podras disfrutar de Los Mejores Videoclip.</description>
 <thumbnail>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</thumbnail>
 <fanart>https://png.pngtree.com/element_origin_min_pic/16/08/30/2057c5817bcf859.jpg</fanart>
-<externallink></externallink>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/scrapertools1.py</externallink>
 </channel>
 </channels>

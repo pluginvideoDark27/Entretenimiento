24d2ebfa597290a9367e3e8a862653d052b16eb0
pluginvideoDark27 <48326077+pluginvideoDark27@users.noreply.github.com>
2019-08-11 14:08:00 +0200

    Update full.py

--- full.py
+++ full.py
@@ -5,6 +5,6 @@
 <description>En este espacio podras disfrutar de Peliculas Y Series</description>
 <thumbnail>https://hdfull.me/templates/hdfull/images/logo.png</thumbnail>
 <fanart>https://www.tuexpertomovil.com/wp-content/uploads/2013/06/moviles-con-pantalla-fullhd-1280x720.jpg</fanart>
-<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/hdfull.py</externallink>
+<externallink>https://raw.githubusercontent.com/pluginvideoDark27/Entretenimiento/master/todos.py</externallink>
 </channel>
 </channels>
